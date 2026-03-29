50693bc4c4f3a68784153101c01984bfa286d909
axense234 <andreicomanescuonline@gmail.com>
2026-03-29 22:39:56 +0300

    Isn't this fun.

--- packages/client/src/app/api/_config/ky.ts
+++ packages/client/src/app/api/_config/ky.ts
@@ -3,6 +3,6 @@ import ky from "ky";
 export const customKy = ky.create({
   prefixUrl: process.env.SERVER,
   headers: {
-    "x-api-key": `"${process.env.SECRET_API_KEY}"`,
+    "x-api-key": process.env.SECRET_API_KEY,
   },
 });
